194722373393b42259cff933546c9c702fb210f7
Calvin Kim <calvinkim4@gmail.com>
2019-04-03 20:05:52 -0400

    Implemented function that checks if player has the max amount of movement

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Player {
 		this.order = 0;
 		this.finished = false;
 		this.dead = false;
-		//this.skipturnAmount = 0;
+		this.finishcount = 0;
 
 	}
 }
@@ -43,15 +43,16 @@ const setRandomEconomicClass = (playerArray) => {
 				break;
 			case 1:
 				playerArray[1].economicClass = 'MiddleClass';
-				playerArray[1].amountOfMoney = 0;
+				playerArray[1].amountOfMoney = 10000;
 				break;
 			case 2:
 				playerArray[2].economicClass = 'MiddleClass';
-				playerArray[2].amountOfMoney = 0;
+				playerArray[2].amountOfMoney = 10000;
+				playerArray[2].position = 29;
 				break;
 			case 3:
 				playerArray[3].economicClass = 'LowerClass';
-				playerArray[3].amountOfMoney = 100;
+				playerArray[3].amountOfMoney = 5000;
 				break;
 			default:
 			break;
@@ -66,11 +67,11 @@ const pickOrder = (playerArray) => {
 	}
 }
 
-let day = 1;
+// let day = 1;
 let nicknameButton = document.getElementById('nickname-button');
 nicknameButton.addEventListener('click', function() {
 	document.querySelector('h1').style.display = 'none';
-	document.querySelector('section > h3').style.display = 'block';
+	// document.querySelector('section > h3').style.display = 'block';
 	for (let i = 0; i < 4; i++) {
 		if (document.querySelectorAll('input')[i].value === '') {
 			setNameOfPlayer('Player ' + (i + 1));
@@ -82,7 +83,7 @@ nicknameButton.addEventListener('click', function() {
 	}
 	setRandomEconomicClass(playerArray);
 	pickOrder(playerArray);
-	document.querySelector('section > h3').innerHTML = 'Day: ' + day;
+	// document.querySelector('section > h3').innerHTML = 'Day: ' + day;
 	nicknameButton.style.display = 'none';
 	showCharacters(playerArray);
 	document.getElementById('roll-div').style.display = 'block';
@@ -130,11 +131,11 @@ const showCharacters = (playerArray) => {
 
 let eventMLArray = [
 // event, bad, good, percentage, money lost from bad, money gained from good, steps back from bad, steps forward from good
-['While jogging, you notice a shady looking grandmother', 'She mugs you', 'You help her mug someone else', 60, 40, -100, 100, 0, 0],
+// ['While jogging, you notice a shady looking grandmother', 'She mugs you', 'You help her mug someone else', 60, 40, -100, 100, 0, 0],
 // ['While walking around your work place, someone coughs on you', 'Your coworker does not apologize and you get sick', 'You get paid time off', 70, 30, -100, 100, -1, 1],
 // ['You worked hard this year and your boss takes notice', 'Just kidding', 'Your boss is feeling generous', 5, 95, 0, 1000, 0, 1],
 // ['Someone hit your car while they were backing up', 'Your car continues to play Ice Ice Baby on repeat', 'It turns out Elon Musk hit you and he gives you a Tesla', 30, 70, -300, 300, -1, 2],
-// ['Your neighbor forgets to turn off the oven while making cereal', 'The flames engulf your apartment', '', 40, 60, -200, 0, 0],
+// ['Your neighbor forgets to turn off the oven while making cereal', 'The flames engulf your apartment', '', 40, 60, -200, 0, -1, 0],
 // ['While at the bus you tied your shoes', 'The bus driver leaves without you', 30, 70, -1, 1],
 // ['Your mother tells you about a company you can invest in', 'It\'s a ponzi scheme', 'It\'s a ponzi scheme but you find other suckers', 80, 20, -1000, 1000, 0, 0],
 // ['The drug dealer you buy weed from asks you to drive weed to another state for money', 'You star in COPS', '', 5, 95, -2000, -2, 1],
@@ -147,7 +148,7 @@ let eventMLArray = [
 // ['You start a band in your friend\'s uncle\'s grandmother\'s garage', 'She has dementia and calls the cops on you', 'You rock hard', 10, 90, -200, 500, 0, 0],
 // ['You apply for General Assembly', 'You don\'t have what it takes', 'You got in', 30, 70, 0, -1500, 0, 2],
 // ['You meet Peter Thiel and try to pitch your idea to him', 'You get too excited and pass out', 'You get series A funding', 1, 99, -500, 100000, 0, 0],
-// ['Your a shut-in because your too powerful', 'You leave but you accidently freeze your sister Anna', 'You become a queen', 40, 60, 0, 0, 0, 1],
+['Your a shut-in because your too powerful', 'You leave but you accidently freeze your sister Anna', 'You become a queen', 40, 60, 0, 0, 0, 1],
 // ['You rule pride rock but your son is being annoying', 'You die', 'Your son grows up to be awesome', 80, 20, 0, 0, -2, 1],
 // ['The prince of Canada']
 ];
@@ -160,20 +161,37 @@ const rollDice = (player) => {
 		document.querySelector('#roll-div > h4').innerHTML = rollNumber;
 	}
 	if (player.economicClass === 'UpperClass') {
-		let rollNumber = Math.floor(Math.random() * 8) + 1;
+		let rollNumber = 30;
 		player.position += rollNumber;
 		document.querySelector('#roll-div > h4').innerHTML = rollNumber;
 	}
 }
 
+let count = 0;
 const movePlayer = (player) => {
 	let setDivContainer = document.querySelector('.step-div-container' + player.order);
 	let containerDivs = setDivContainer.children;
 	let playerDiv = document.getElementById('player-div' + player.order);
-	if (player.position > containerDivs.length - 1) {
+	if (player.position >= containerDivs.length - 1) {
+		containerDivs[containerDivs.length - 1].appendChild(playerDiv);
+		player.finished = true;
+		count++;
+		player.finishcount = count;
+	} else {
+		//console.log(player.position);
+		containerDivs[player.position].appendChild(playerDiv);
+	}
+}
+
+const movePlayerWOCount = (player) => {
+	let setDivContainer = document.querySelector('.step-div-container' + player.order);
+	let containerDivs = setDivContainer.children;
+	let playerDiv = document.getElementById('player-div' + player.order);
+	if (player.position >= containerDivs.length - 1) {
 		containerDivs[containerDivs.length - 1].appendChild(playerDiv);
 		player.finished = true;
 	} else {
+		//console.log(player.position);
 		containerDivs[player.position].appendChild(playerDiv);
 	}
 }
@@ -196,10 +214,12 @@ const pickRandomEvent = (eventArray, player) => {
 
 const nextTurn = () => {
 	// let playerTurn = playerArray[0];
-	if (!playerArray[0].finished) {
-		rollDice(playerArray[0]);
-		movePlayer(playerArray[0]);
-	}
+	rollDice(playerArray[0]);
+	movePlayer(playerArray[0]);
+	// if (!playerArray[0].finished) {
+	// 	rollDice(playerArray[0]);
+	// 	movePlayer(playerArray[0]);
+	// }
 	// playerArray.splice(0, 1);
 	// playerArray.push(playerTurn);
 }
@@ -215,13 +235,77 @@ const showEvent = (randomEvent) => {
 }
 
 const setEventEffect = (randomEvent, player) => {
-	if (randomEvent[3] < Math.floor(Math.random() * 100)) {
-		player.position += randomEvent[8];
+	if (Math.floor(Math.random() * 100) <= randomEvent[3]) {
+		player.position += randomEvent[7];
 		player.amountOfMoney += randomEvent[5];
 	} else {
-		player.position += randomEvent[7];
+		player.position += randomEvent[8];
 		player.amountOfMoney += randomEvent[6];
 	}
+}
+
+// let firstFinished;
+// const checkFirstFinished = (playerArray) => {
+// 	for (let i = 0; i < playerArray.length; i++) {
+// 		if (playerArray[i].finishcount === 1) {
+// 			firstFinished = playerArray[i];
+// 			if (!playerArray[0].finished) {
+// 					playerArray[0].amountOfMoney -= 100;
+// 					playerArray[i].amountOfMoney += 100;
+// 				}
+// 			// let firstFinished = playerArray[i];
+// 			// for (let i = 0; i < playerArray.length; i++) {
+// 			// 	if (!playerArray[i].finished) {
+// 			// 		playerArray[i].amountOfMoney -= 100;
+// 			// 		firstFinished.amountOfMoney += 100;
+// 			// 	}
+// 			// }
+				
+// 			//let playerFirst = playerArray[i];
+// 			// for (let i = 0; i < playerArray.length; i++) {
+// 			// 	if (!playerArray[i].finished) {
+// 			// 		playerArray[i].amountOfMoney -= 100;
+// 			// 		playerFirst.amountOfMoney += 100;
+// 			// 	}
+// 			// }
+// 		}
+// 		document.querySelectorAll('.money-amt-class')[playerArray[i].order].innerHTML = '$' + playerArray[i].amountOfMoney;
+
+// 	}
+	
+// }
+
+
+const checkAllFinished = () => {
+	// let count = 0;
+	// for (let i = 0; i < playerArray.length; i++) {
+	// 	if (playerArray[i].finished) {
+	// 		count++;
+	// 		playerArray[i].finishcount = count;
+	// 	}
+	// }
+	//checkFirstFinished(playerArray);
+	if (count === playerArray.length) {
+		for (let i = 0; i < playerArray.length; i++) {
+			givePayDayForFinished(playerArray[i]);
+			document.querySelectorAll('.money-amt-class')[playerArray[i].order].innerHTML = '$' + playerArray[i].amountOfMoney;
+			playerArray[i].finished = false;
+			playerArray[i].finishcount = 0;
+			playerArray[i].position = 0;
+			movePlayerWOCount(playerArray[i]);
+		}
+		count = 0;
+	}
+}
+
+const givePayDayForFinished = (player) => {
+	if (player.economicClass === 'UpperClass') {
+				player.amountOfMoney += 100000;
+			} else if (player.economicClass === 'MiddleClass') {
+				player.amountOfMoney += 5000;
+			} else {
+				player.amountOfMoney += 3000;
+			}
 }
 
 const checkWinner = () => {
@@ -230,57 +314,93 @@ const checkWinner = () => {
 	 }
 }
 
+const firstUnfinished = (player) => {
+	return player.finished === false;
 
-let turns = 0;
+}
+
+let turns = 1;
+let reset = false;
 const playGame = () => {
+	document.querySelectorAll('.name-h3')[playerArray[0].order].style.color = 'black';
 	let chosenEvent;
 	let rollButton = document.getElementById('roll-button');
 	rollButton.addEventListener('click', function () {
 		chosenEvent = pickRandomEvent(eventMLArray, playerArray[0]);
+		//window.setTimeout(nextTurn, 1000);
 		nextTurn();
+		//window.setTimeout(showEvent(chosenEvent), 2000);
+		//nextTurn();
 		showEvent(chosenEvent);	
-		turns++;
-		if (turns % playerArray.length === 0) {
-		day++;
-		document.querySelector('section > h3').innerHTML = 'Day: ' + day;
-	}
 	});
 	let chooseButton = document.getElementById('event-roll-button');
 		chooseButton.addEventListener('click', function() {
 		setEventEffect(chosenEvent, playerArray[0]);
-		document.querySelectorAll('.money-amt-class')[playerArray[0].order].innerHTML = playerArray[0].amountOfMoney;
+		movePlayerWOCount(playerArray[0]);
+		for (let i = 0; i < playerArray.length; i++) {
+			document.querySelectorAll('.money-amt-class')[playerArray[i].order].innerHTML = '$' + playerArray[i].amountOfMoney;
+		}
+
+		turns++;
+		if (turns % playerArray.length === 0) {
+			turns = 0;
+			//reset = true;
+			// day++;
+			// document.querySelector('section > h3').innerHTML = 'Day: ' + day;
+		}
+
+		//console.log(reset);
+
+		const unfinishedArray = playerArray.filter(player => player.finished === false);
+
+		if (turns === 0) {
+			let firstFinished;
+			for (let i = 0; i < playerArray.length; i++) {
+				if (playerArray[i].finishcount === 1) {
+					firstFinished = playerArray[i];
+					for (let i = 0; i < playerArray.length; i++) {
+						if (!playerArray[i].finished) {
+							playerArray[i].amountOfMoney -= 100;
+							firstFinished.amountOfMoney += 100;
+						}
+					}
+				}
+			}
+		}
+		
+		
 		document.querySelector('.modal-background').style.display = 'none';
+		document.querySelectorAll('.name-h3')[playerArray[0].order].style.color = '#E8FCCF';
 		playerArray.push(playerArray[0]);
 		playerArray.splice(0, 1);
-		// console.log(playerArray);
 		for (let i = playerArray.length - 1; i >= 0; i--) {
 			if (playerArray[i].amountOfMoney <= 0) {
 			playerArray[i].dead = true;
 			playerArray.splice(i, 1);
-			// console.log('spliced');
-			// console.log(playerArray);
-		} 
-		//console.log(playerArray);
+			} 
 		}
 		
+		let nextUnfinished = playerArray.findIndex(firstUnfinished);
+		//console.log(nextUnfinished);
+
+		for (let i = 0; i < nextUnfinished; i++){
+			document.querySelectorAll('.name-h3')[playerArray[0].order].style.color = '#E8FCCF';
+			playerArray.push(playerArray[0]);
+			playerArray.splice(0, 1);
+			document.querySelectorAll('.name-h3')[playerArray[0].order].style.color = 'black';
+		}
+
+		document.querySelectorAll('.name-h3')[playerArray[0].order].style.color = 'black';
+		
+		checkAllFinished();
+		// if (turns % (4 - count) === 0) {
+		// 	checkFirstFinished(playerArray);
+		// }
+		//console.log(turns % playerArray.length);
+		//checkFirstFinished(playerArray);
 		checkWinner();
-	// if (!playerArray[0].dead) {
-	// 	let chooseButton = document.getElementById('event-roll-button');
-	// 	chooseButton.addEventListener('click', function() {
-	// 	setEventEffect(chosenEvent, playerArray[0]);
-	// 	//console.log(playerArray[0]);
-	// 	//console.log(document.querySelectorAll('.money-amt-class')[playerArray[0].order].innerHTML);
-	// 	document.querySelectorAll('.money-amt-class')[playerArray[0].order].innerHTML = playerArray[0].amountOfMoney;
-	// 	document.querySelector('.modal-background').style.display = 'none';
-	// 	playerArray.push(playerArray[0]);
-	// 	playerArray.splice(0, 1);
+		//console.log(playerArray);
 	});
-	// } else {
-	// 	document.querySelector('.modal-background').style.display = 'none';
-	// 	playerArray.push(playerArray[0]);
-	// 	playerArray.splice(0, 1);
-	// }
-	
 }
 
 const resetGame = () => {
